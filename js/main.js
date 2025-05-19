9629fcbb2f517db004548307496ae8ec17ae4fbb
litvinenko <litvinenko_n.v@mail.ru>
2025-05-19 13:50:21 +0300

    Часть адаптирована 1 блока

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".mobile-menu-toggle");
+  const navList = document.querySelector(".list-nav");
+  const body = document.body;
+
+  menuToggle.addEventListener("click", function () {
+    navList.classList.toggle("active");
+    this.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+  });
+
+  // Закрытие меню при клике на пункт
+  document.querySelectorAll(".list-nav__item").forEach((item) => {
+    item.addEventListener("click", function () {
+      navList.classList.remove("active");
+      menuToggle.classList.remove("active");
+      body.classList.remove("no-scroll");
+    });
+  });
+});
